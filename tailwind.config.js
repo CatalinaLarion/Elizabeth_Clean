ce1349b2ffc68603b9d7a2800bd7b640d569f036
Catalina Larion <larion.catalina@gmail.com>
2022-10-21 21:25:07 +0300

    added files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./assets/*.liquid",
-    "./layout/*.liquid",
-    "./sections/*.liquid",
-    "./snippets/*.liquid",
-    "./templates/*.liquid",
-    "./templates/customers/*.liquid",
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
